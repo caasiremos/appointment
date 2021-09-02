be434e066fc9b8a5e63253ae2b516bd50b2a77ab
caasiremos <aremoisaac@gmail.com>
2021-09-03 01:49:44 +0300

    added booking controller and view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/bookings/create', BookingController::class);
 
 require __DIR__.'/auth.php';
