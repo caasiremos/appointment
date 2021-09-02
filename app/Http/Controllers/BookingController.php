878192579906e876f5c9a665b49309157ca8af3f
caasiremos <aremoisaac@gmail.com>
2021-09-03 02:58:17 +0300

    added time interval logic

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Schedule;
+use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
+use App\Utils\Bookings\TimeSlotGenerator;
 
 class BookingController extends Controller
 {
     public function __invoke()
     {
-        return view('bookings.create');
+        $schedule = Schedule::findOrFail(1);
+
+        $slots = (new TimeSlotGenerator($schedule))->get();
+
+        return view('bookings.create', compact('slots'));
     }
 }
