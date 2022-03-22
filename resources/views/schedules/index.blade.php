c796ee2cd1dd19b1b7a5d1033b32dd4cb2338321
caasiremos <aremoisaac@gmail.com>
2022-03-22 12:55:04 +0300

    remove schedule scoping for admin role

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -11,7 +11,6 @@
             </a>
         </div>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -24,6 +23,10 @@
                                     <table class="min-w-full divide-y divide-gray-200">
                                         <thead class="bg-gray-50">
                                         <tr>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                
+                                            </th>
                                             <th scope="col"
                                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                 Date
